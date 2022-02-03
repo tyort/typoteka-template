313c5d1ece1796bf1b6f3a1b21e1f753ff0f2d22
tyort <neotyo1986@gmail.com>
2022-02-03 19:09:46 +0300

    Общие формулы и константы

---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+export const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const generateDateByMs = () => {
+  const milliseconds = getRandomInt(1443903114233, 1643903114233);
+  return new Date(milliseconds);
+};
+
+export const getShuffledArray = (someArray: string[]) => {
+  for (let i = someArray.length - 1; i > 0; i--) {
+    const randomPosition = Math.floor(Math.random() * i);
+    [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
+  }
+
+  return someArray;
+};
