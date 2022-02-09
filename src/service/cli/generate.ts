b75e93dc5787b49e4a4b8bb274a69f517b8f1520
tyort <neotyo1986@gmail.com>
2022-02-09 10:16:32 +0300

    Рефакторинг generate.ts. Исправление версии chalk под TypeScript

--- src/service/cli/generate.ts
+++ src/service/cli/generate.ts
@@ -1,12 +1,17 @@
 import dayjs from "dayjs";
 import chalk from "chalk";
-import fs from "fs";
-import { categories, announceParts, titles, DEFAULT_COUNT, FILE_NAME} from "../../const";
+import {promises as fs} from "fs";
+import { DEFAULT_COUNT, FILE_NAME} from "../../const";
 import { Publication } from "../../types";
 import { getRandomInt, getShuffledArray, generateDateByMs } from "../../utils";
 
-const generatePublications = (count: number): Publication[] => (
-  Array(count)
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+
+const generatePublications = (count: number, mockContent: {[propertyName: string]: string[]}): Publication[] => {
+  const {categories, sentences: announceParts, titles} = mockContent;
+  return Array(count)
     .fill({})
     .map(() => ({
       category: [categories[getRandomInt(0, categories.length - 1)]],
@@ -17,24 +22,35 @@ const generatePublications = (count: number): Publication[] => (
       createdDate: dayjs(generateDateByMs()).format(`YYYY-MM-DD HH:mm:ss`),
       title: titles[getRandomInt(0, titles.length - 1)],
     }))
-);
+  };
+
+const readContent = async (filePath: string) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
 const generate = {
   name: `--generate`,
   // args - параметр получаемый от пользователя
-  run(args: string[]) {
+  async run(args: string[]) {
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
     const [count] = args; // count - первый элемент массива
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generatePublications(countOffer));
+    const content = JSON.stringify(generatePublications(countOffer, {sentences, titles, categories}));
 
-    // fs - имеет различные функции для выполнения операций с файловой системой:
-    // создание, чтение, запись, удаление и так далее.
-    fs.writeFile(`src/${FILE_NAME}`, content, (err) => {
-      return err
-        ? console.error(chalk.red(`Can't write data to file...`))
-        : console.log(chalk.green(`Operation success. File created.`));
-    });
-
+    try {
+      await fs.writeFile(`src/${FILE_NAME}`, content);
+      console.log(chalk.green(`Operation success. File created.`));
+    } catch (err) {
+      console.error(chalk.red(`Can't write data to file...`));
+    }
   }
 };
 
