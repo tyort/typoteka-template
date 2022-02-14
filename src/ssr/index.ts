b769ab19f3f6b65de11c025fe8b21f3d28954216
tyort <neotyo1986@gmail.com>
2022-02-14 19:15:06 +0300

    Настраиваем express на обработку файлов .pug

--- src/ssr/index.ts
+++ src/ssr/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import articlesRoutes from "./routes/articles";
 import myRoutes from "./routes/my";
 import mainRoutes from "./routes/main";
+import path from "path";
 
 const DEFAULT_PORT = 8080;
 
@@ -10,6 +11,9 @@ const app = express();
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
+
+app.set(`views`, path.resolve(__dirname, `templates`)); // путь к директории, в которой будут храниться все наши pug-шаблоны
+app.set(`view engine`, `pug`);
 
 // Запуск сервера
 app.listen(DEFAULT_PORT);
