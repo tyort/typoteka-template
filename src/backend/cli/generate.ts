2b52296f58c33e787fd765c804aab48c87913cff
tyort <neotyo1986@gmail.com>
2022-02-22 01:25:26 +0300

    Генерируем усовершенствованные публикации

--- src/backend/cli/generate.ts
+++ src/backend/cli/generate.ts
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import chalk from "chalk";
-import {promises as fs} from "fs";
+import { nanoid } from "nanoid";
+import { promises as fs } from "fs";
 import { DEFAULT_COUNT, FILE_NAME} from "../../const";
 import { Publication } from "../../types";
 import { getRandomInt, getShuffledArray, generateDateByMs } from "../../utils";
@@ -8,12 +9,23 @@ import { getRandomInt, getShuffledArray, generateDateByMs } from "../../utils";
 const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+const FILE_COMMENTS_PATH = `./data/comments.txt`;
+
+const generateComments = (count: number, comments: string[]) => (
+  Array(count).fill({}).map(() => ({
+    id: nanoid(6),
+    text: getShuffledArray(comments)
+      .slice(0, getRandomInt(1, 3))
+      .join(` `),
+  }))
+);
 
 const generatePublications = (count: number, mockContent: {[propertyName: string]: string[]}): Publication[] => {
-  const {categories, sentences: announceParts, titles} = mockContent;
+  const {categories, sentences: announceParts, titles, comments} = mockContent;
   return Array(count)
     .fill({})
     .map(() => ({
+      id: nanoid(6),
       category: [categories[getRandomInt(0, categories.length - 1)]],
       fullText: getShuffledArray(announceParts)
         .slice(1, getRandomInt(0, announceParts.length - 1))
@@ -21,6 +33,7 @@ const generatePublications = (count: number, mockContent: {[propertyName: string
       announce: getShuffledArray(announceParts).slice(1, 5).join(` `),
       createdDate: dayjs(generateDateByMs()).format(`YYYY-MM-DD HH:mm:ss`),
       title: titles[getRandomInt(0, titles.length - 1)],
+      comments: generateComments(getRandomInt(1, 4), comments)
     }))
   };
 
@@ -41,9 +54,10 @@ const generate = {
     const sentences = await readContent(FILE_SENTENCES_PATH);
     const titles = await readContent(FILE_TITLES_PATH);
     const categories = await readContent(FILE_CATEGORIES_PATH);
+    const comments = await readContent(FILE_COMMENTS_PATH);
     const [count] = args; // count - первый элемент массива
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generatePublications(countOffer, {sentences, titles, categories}));
+    const content = JSON.stringify(generatePublications(countOffer, {sentences, titles, categories, comments}));
 
     try {
       await fs.writeFile(`src/${FILE_NAME}`, content);
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,14 @@
 export type Publication = {
+  id: string,
   announce: string,
   title: string,
   fullText: string,
   createdDate: string,
-  category: string[]
-};
+  category: string[],
+  comments: Comment[]
+}
+
+export type Comment = {
+  id: string,
+  text: string
+}
