6de578bae4ab9f2d7b7a70adcf0c6c02eddeb016
tyort <neotyo1986@gmail.com>
2022-03-09 07:43:05 +0300

    Рефакторим логгер

--- src/backend/library/logger.ts
+++ src/backend/library/logger.ts
@@ -1,10 +1,20 @@
 import pino from "pino";
 
+// ????? Почему путь работает относительно корневой папки
+const LOG_FILE = `./logs/api.log`;
+
+const onPinoPretty = {
+  target: `pino-pretty`,
+  options: {
+    colorize: true
+  }
+}
+
 export const logger = pino({
   name: `base-logger`,
-  level: `debug`,
-  prettyPrint: true
-});
+  level: process.env.NODE_ENV_DEV ? `debug` : `info`,
+  transport: process.env.NODE_ENV_DEV ? onPinoPretty : undefined,
+}, process.env.NODE_ENV_DEV ? process.stdout : pino.destination(LOG_FILE));
 
 // Метод всегда возвращает новый логгер, унаследованный от стандартного логгера.
 // В метод можно передать специфичные настройки для нового экземпляра класса.
