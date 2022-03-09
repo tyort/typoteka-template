5dd9ef1443eea69dc830850449e56c003d8fc147
tyort <neotyo1986@gmail.com>
2022-03-09 07:44:07 +0300

    Рефакторим сервер на бэкэнде

--- src/backend/cli/server.ts
+++ src/backend/cli/server.ts
@@ -1,37 +1,56 @@
 import chalk from "chalk";
 import express from "express";
-import {promises} from "fs";
+import { ErrorRequestHandler } from "express-serve-static-core";
+import { getLogger } from "../library/logger";
 import { HttpCode } from "../../const";
-import { Publication } from "../../types";
-import path from "path";
+import routes from "../routes";
 
+const logger = getLogger({name: `backend-server`});
 const DEFAULT_PORT = 3005;
-const FILENAME = `mocks.json`;
 
 const app = express();
 app.use(express.json()); // express.json() - middleware решит задачу конвертации JSON содержимого в объекты JavaScript.
 
-app.get(`/posts`, async (req, res) => {
-  try {
-    const fileContent = await promises.readFile(path.resolve(__dirname, `../../${FILENAME}`));
-    const mocks: Publication[] = JSON.parse(fileContent.toString() as string);
-    res.json(mocks);
-
-  } catch (_err) {
-    res.send([]);
-  }
+// Фиксируем вообще все запросы к API и коды ответа на них.
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    const color = res.statusCode >= 400 ? `red` : `green`;
+    logger.info(chalk[color](`Response status code ${res.statusCode}`))
+  });
+  next();
 });
 
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
+app.use(`/api`, routes);
+
+// Фиксируем события, когда происходит запрос на несуществующий маршрут.
+app.use((req, res) => {
+  res
+    .status(HttpCode.NOT_FOUND)
+    .send(`Not found`);
+  logger.error(chalk.red(`Route not found: ${req.url}`));
+});
+
+const errorRequestMiddleware: ErrorRequestHandler = (err, _req, _res, _next) => {
+  logger.error(`An error occured on processing request: ${err.message}`);
+};
+
+app.use(errorRequestMiddleware);
 
 export default {
   name: `--server`,
-  run(args: string[]) {
+  async run(args: string[]) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    app.listen(port, () => console.info(chalk.green(`Ожидаю соединений на ${port}`)))
+    try {
+      app.listen(port, () => {
+          logger.info(chalk.green(`Listening to connections on ${port}`));
+      });
+
+    } catch (err) {
+      logger.error(chalk.red(`An error occurred: ${(err as Error).message}`));
+      process.exit(1);
+    }
   }
-}
+};
