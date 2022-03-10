236bf1b520f2db1dc46c3be3d6065a7e54562cb8
tyort <neotyo1986@gmail.com>
2022-03-10 13:34:06 +0300

    Подключение и рефакторинг main.pug

--- src/frontend/routes/main.ts
+++ src/frontend/routes/main.ts
@@ -1,7 +1,32 @@
 import { Router } from "express";
+import { getAPI } from "../api";
+import { Publication } from "../../types";
+import dayjs from "dayjs";
+
+const api = getAPI();
 const mainRouter = Router();
 
-mainRouter.get(`/`, (req, res) => res.send(`main`));
+mainRouter.get(`/`, async (req, res) => {
+  const [articles, categories] = await Promise.all([
+    api.getArticles(),
+    api.getCategories()
+  ]);
+
+  const getFormatedDate = (createdDate: string, formatDate: string): string => {
+    return dayjs(createdDate).format(formatDate);
+  }
+  const getCountByCategory = (category: string): number => {
+    return articles.filter((article: Publication) => article.category.includes(category)).length;
+  }
+
+  res.render(`main`, {
+    articles: articles as Publication[],
+    categories,
+    getFormatedDate,
+    getCountByCategory
+  });
+});
+
 mainRouter.get(`/register`, (req, res) => res.send(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.send(`login`));
 mainRouter.get(`/search`, (req, res) => res.send(`search-result`));
