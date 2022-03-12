5df9d7bca37339cb32915a936e7f40e617f13589
tyort <neotyo1986@gmail.com>
2022-03-12 14:21:07 +0300

    Рефакторинг publication-empty.pug

--- src/frontend/routes/articles.ts
+++ src/frontend/routes/articles.ts
@@ -6,9 +6,16 @@ import { Publication } from "../../types";
 const api = getAPI();
 const articlesRouter = Router();
 
-articlesRouter.get(`/:id`, (req, res) => res.send(`Иди жопу нюхай`));
 articlesRouter.get(`/category/:id`, (req, res) => res.send(`Говно ебаное`));
-articlesRouter.get(`/add`, (req, res) => res.send(`Жри сука`));
+
+articlesRouter.get(`/add`, async (req, res) => {
+  const getFormatedDate = (createdDate: string, formatDate: string): string => {
+    return dayjs(createdDate).format(formatDate);
+  };
+  res.render(`publications/publication-empty`, {getFormatedDate});
+});
+
+articlesRouter.get(`/:id`, (req, res) => res.send(`Иди жопу нюхай`));
 
 articlesRouter.get(`/edit/:articleId`, async (req, res) => {
   const {articleId} = req.params;
