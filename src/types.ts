ea1fcd8299ee65e1898adc8a57bc3f77476f7122
tyort <neotyo1986@gmail.com>
2022-05-01 12:56:12 +0300

    Рефакторинг типов

--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,12 @@
-// ?????? Разобраться с типами
-export type Publication = {
-  id: string,
+export type ArticleAttributes = {
   picture: string,
   announce: string,
   title: string,
   fullText: string,
-  createdDate: string,
-  category: string[],
+  categories: string[],
   comments: Comment[]
 }
 
-// ?????? Разобраться с типами
-export type ArticleAttributes = {
-  id: number,
-  picture: string,
-  announce: string,
-  title: string,
-  fullText: string,
-  createdAt: Date,
-  updatedAt: Date,
-}
-
 export type Comment = {
-  id: string,
   text: string
 }
